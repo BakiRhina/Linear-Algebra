0e334a9efb672abc19a67e70fe5bba67d42caf08
Baki <jdrembed@gmail.com>
2022-07-03 18:59:31 +0200

    Deleting sample file and adding a new python file

---
+++ Matrices and vectors (numpy).py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Matrices and vectors
+# *** 
+# We are going to learn about vectors and matrices and how to implement them with pythom
+
+# In[5]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+A = np.matrix([[1,2],[3,4]])
+
+
+# In[7]:
+
+
+B = 5 + A
+
+
+# In[ ]:
+
+
+
+
